251c0d3a68b3e999d460a43edbe15b9fd992daa4
ShanYong456 <sooshanyong456@gmail.com>
2020-08-24 21:09:27 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c346/id18015938/demosmstest/MainActivity.java
@@ -0,0 +1,63 @@
+package sg.edu.rp.c346.id18015938.demosmstest;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.Manifest;
+import android.content.BroadcastReceiver;
+import android.content.IntentFilter;
+import android.content.pm.PackageManager;
+import android.net.ConnectivityManager;
+import android.os.Bundle;
+import android.provider.Telephony;
+import android.telephony.SmsManager;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button sendbtn;
+    private BroadcastReceiver br;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        checkPermission();
+
+        br = new MessageReceiver();
+
+        IntentFilter filter = new IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION);
+        filter.addAction("android.provider.Telephony.SMS_RECEIVED");
+        this.registerReceiver(br, filter);
+        //Other Codes
+
+        sendbtn = findViewById(R.id.button);
+
+        sendbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SmsManager smsManager = SmsManager.getDefault();
+                smsManager.sendTextMessage("5556", null, "Hello World", null, null);
+            }
+        });
+
+
+
+    }
+
+    private void checkPermission () {
+        int permissionSendSMS = ContextCompat.checkSelfPermission(this,
+                Manifest.permission.SEND_SMS);
+        int permissionRecvSMS = ContextCompat.checkSelfPermission(this,
+                Manifest.permission.RECEIVE_SMS);
+        if (permissionSendSMS != PackageManager.PERMISSION_GRANTED &&
+                permissionRecvSMS != PackageManager.PERMISSION_GRANTED) {
+            String[] permissionNeeded = new String[]{Manifest.permission.SEND_SMS,
+                    Manifest.permission.RECEIVE_SMS};
+            ActivityCompat.requestPermissions(this, permissionNeeded, 1);
+        }
+
+    }
+}
